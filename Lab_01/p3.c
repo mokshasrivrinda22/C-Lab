3627c28cc5298d55ac157cfbfbac102225762518
mokshasrivrinda22 <mokshavch@gmail.com>
2026-09-17 20:55:55 +0530

    Add p3.c for character to float conversion
    Implement character to float conversion in p3.c

---
+++ Lab_01/p3.c
@@ -0,0 +1,9 @@
+//Converting character datatype into float datatype
+#include <stdio.h>
+int main() 
+{
+    char firstChar = 'V';               
+    float convertedCount = firstChar; 
+    printf("The converted float value is %f\n", convertedCount);
+    return 0;
+}
